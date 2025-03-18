09ebc491074d8b7180a583b7c96e5350bdedcc54
mcid9 <matiascidcatalan@gmail.com>
2025-03-17 21:53:43 -0300

    Guardar amigos en array

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo').value;
+    
+    //Comprobar si input amigo está vacío.
+    if (!inputAmigo) {
+        alert('Debe ingresar un amigo');
+    } else {
+        amigos.push(inputAmigo);
+        //console.log(amigos);
+        limpiarInput();
+    }
+}
+
+function limpiarInput() {
+    document.getElementById('amigo').value = '';
+}
